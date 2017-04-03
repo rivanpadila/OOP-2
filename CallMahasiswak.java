fe3ddab3e74a214d5657f780aef4e8a3ed98460a
rivanpadila <rivanpadila910@gmail.com>
2017-04-03 21:41:40 +0700

    Add files via upload

---
+++ CallMahasiswak.java
@@ -0,0 +1,6 @@
+public class CallMahasiswak {
+	public static void main (String[] args) {
+		Mahasiswak mhs = new Mahasiswak();
+		System.out.println(mhs.nama);
+	}
+}
---
+++ Mahasiswak.java
@@ -0,0 +1,7 @@
+public class Mahasiswak {
+	String nama;
+	
+	Mahasiswak() { //konstruktor
+	nama = "java";
+	}
+}
